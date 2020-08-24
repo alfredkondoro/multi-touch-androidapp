8e7ca90773cd0211587d9d0bba0d87309c7287e9
Alfred Kondoro <alfredkondoro97@gmail.com>
2020-08-24 15:40:06 +0300

    version 1.0

--- app/src/main/java/com/alfredkondoro/multi_touch/MainActivity.java
+++ app/src/main/java/com/alfredkondoro/multi_touch/MainActivity.java
@@ -3,6 +3,10 @@ package com.alfredkondoro.multi_touch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        RelativeLayout mLayout = (RelativeLayout) findViewById (R.id.mLayout);
+
+        mLayout.setOnTouchListener (new RelativeLayout.OnTouchListener () {
+            public boolean onTouch(View v, MotionEvent m) {
+                handleTouch(m);
+                return true;
+            }
+        });
+    }
+
+    private void handleTouch(MotionEvent m) {
+        TextView firstTouch  = (TextView) findViewById (R.id.firstTouch);
+        TextView secondTouch = (TextView) findViewById (R.id.secondTouch);
+
+        int pointerCount = m.getPointerCount ();
+
+        for(int i=0; i<pointerCount; i++){
+            int x = (int) m.getX(i);
+            int y = (int) m.getY(i);
+            int id = m.getPointerId (i);
+
+            String touchStatus = " ID " + id + "X: " + x + "Y: " +y;
+            if(id==0)
+                firstTouch.setText (touchStatus);
+            else
+                secondTouch.setText (touchStatus);
+        }
     }
 }
